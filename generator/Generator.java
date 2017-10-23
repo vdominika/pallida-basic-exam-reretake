b7317786558cc626315d75f3f339b4891564dd6c
aze <adam.gyulavari@lab.coop>
2017-10-23 20:21:16 +0200

    feat(reretake-exam) add exercises for java

---
+++ generator/Generator.java
@@ -0,0 +1,11 @@
+public class Generator {
+    public static void main(String[] args) {
+        // Create a function that takes a whole number
+        // as parameter and generates a list containing
+        // all the FizzBuzz numbers smaller than that.
+        // FizzBuzz number is divideable by 3 or 5
+        // System.out.println(generator(20));
+        // should print out:
+        // [3, 5, 6, 9, 10, 12, 15, 18, 20]
+    }
+}
---
+++ merger/Merger.java
@@ -0,0 +1,13 @@
+public class Merger {
+    public static void main(String[] args) {
+        // Create a function that takes two string list as parameters
+        // and merges the two list into one.
+        // Putting the even elements from the first
+        // and the odd ones from the second list.
+        // System.out.println(merger(Arrays.asList("kutya", "macska", "kecske"),
+        //                           Arrays.asList("bokor", "tolgy", "fenyo")));
+        // should print out
+        // ["bokor", "kutya", "tolgy", "macska", "fenyo", "kecske"]
+        // Create 3 different unit tests for the function!
+    }
+}
