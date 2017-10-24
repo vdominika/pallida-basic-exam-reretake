82359109fd97d85ae075283647c02bfb1c400b20
vdominika <vdominika@yahoo.com>
2017-10-24 16:44:28 +0200

    Created method

--- src/Generator.java
+++ src/Generator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Generator {
@@ -6,13 +8,21 @@ public class Generator {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Please enter a number, which is higher than 0");
         int inputNumber = myScanner.nextInt();
- //       System.out.println(generator(20));
+        System.out.println(generator(inputNumber));
     }
 
-    public static void generator(int inputNumber){
 
+    public static List<Integer> generator(int inputNumber) {
+            List<Integer> result = new ArrayList<>();
+
+            for (int i = 1; i <= inputNumber; i++) {
+                if (i % 3 == 0 || i % 5 == 0)
+                    result.add(i);
+            }
+            return result;
+        }
     }
-}
+
 
 // Create a function that takes a whole number
 // as parameter and generates a list containing
@@ -21,3 +31,4 @@ public class Generator {
 // System.out.println(generator(20));
 // should print out:
 // [3, 5, 6, 9, 10, 12, 15, 18, 20]
+
